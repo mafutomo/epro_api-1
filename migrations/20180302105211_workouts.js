f38878f1145c3becfd9f9337d7284822999d09b0
Stephanie Marvez <marvez@gmail.com>
2018-03-03 15:33:45 -0700

    updated db tables with lisa's input

--- migrations/20180302105211_workouts.js
+++ migrations/20180302105211_workouts.js
@@ -7,6 +7,8 @@ exports.up = function(knex, Promise) {
     table.integer('trainer_id').notNullable()
     table.foreign('trainer_id').references('users.id').onDelete('CASCADE')
     table.string('date').notNullable()
+    table.string('name')
+    table.boolean('created_by_trainer').notNullable()
     table.timestamp('created_at').notNullable().defaultsTo(knex.raw('now()'))
     table.timestamp('updated_at').notNullable().defaultsTo(knex.raw('now()'))
   })
