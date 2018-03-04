c362802696583d5d24bd12f8d08e5d30ac7f2f07
Melissa Utomo <mafutomo@gmail.com>
2018-03-03 23:21:15 -0700

    added hormone routes, add user, and workouts

--- src/queries/queries.js
+++ src/queries/queries.js
@@ -4,14 +4,39 @@ const knex = require('../../knex')
 const bodyParser = require('body-parser')
 
 
-const getAllUsers = (req, res, next) => {
-  knex('users').then(data => {
-    res.status(200).send(data)
+const getAllMonophasic = (req, res, next) => {
+  knex('monophasic_hormones')
+    .then(data => {
+      res.status(200).send(data)
   })
 }
 
-const getHormones = (req, res, next) => {
-  console.log('getHormones')
+const getAllNonHormonal = (req, res, next) => {
+  knex('non_hormonal')
+    .then(data => {
+      res.status(200).send(data)
+  })
+}
+
+const getAllTriphasic = (req, res, next) => {
+  knex('triphasic_hormones')
+    .then(data => {
+      res.status(200).send(data)
+  })
+}
+
+const getAllProgestin = (req, res, next) => {
+  knex('progestin_hormones')
+    .then(data => {
+      res.status(200).send(data)
+  })
+}
+
+const getAllUsers = (req, res, next) => {
+  knex('users')
+    .then(data => {
+      res.status(200).send(data)
+  })
 }
 
 const getAllTrainers = (req, res, next) => {
@@ -24,11 +49,69 @@ const getAllTrainers = (req, res, next) => {
 }
 
 const getAllWorkoutsForUser = (req, res, next) => {
-  console.log('getAllWorkoutsForUser');
+  knex('workouts').where({
+    client_id: req.params.id
+  })
+  .then(data => {
+    res.status(200).send(data)
+  })
 }
 
-const getWorkoutsForUserByDate = (req, res, next) => {
-  console.log('getWorkoutsForUserByDate');
+const getAllPhaseTips = (req, res, next) =>{
+  knex('phase_tips')
+  .then(data => {
+    res.status(200).send(data)
+  })
+}
+
+const getMonophasicById = (req, res, next) => {
+  knex('monophasic_hormones')
+  .where({
+    id: req.params.id
+  })
+  .then(data => {
+    res.status(200).send(data)
+  })
+}
+
+const getNonHormonalById = (req, res, next) => {
+  knex('non_hormonal')
+  .where({
+    id: req.params.id
+  })
+  .then(data => {
+    res.status(200).send(data)
+  })
+}
+
+const getTriphasicById = (req, res, next) => {
+  knex('triphasic_hormones')
+  .where({
+    id: req.params.id
+  })
+  .then(data => {
+    res.status(200).send(data)
+  })
+}
+
+const getProgestinById = (req, res, next) => {
+  knex('progestin_hormones')
+  .where({
+    id: req.params.id
+  })
+  .then(data => {
+    res.status(200).send(data)
+  })
+}
+
+const getPhaseTipsById = (req, res, next) =>{
+  knex('phase_tips')
+  .where({
+    id: req.params.id
+  })
+  .then(data => {
+    res.status(200).send(data)
+  })
 }
 
 const getUserByID = (req, res, next) => {
@@ -38,6 +121,11 @@ const getUserByID = (req, res, next) => {
   .then(data => {
     res.status(200).send(data)
   })
+}
+
+const getWorkoutsForUserByDate = (req, res, next) => {
+  console.log(req.params)
+  console.log('getWorkoutsForUserByDate');
 }
 
 const getTrainerByID = (req, res, next) => {
@@ -51,28 +139,51 @@ const getTrainerByID = (req, res, next) => {
 }
 
 const createUser = (req, res, next) => {
-  console.log('createUser')
   // bcrypt stuff
   // let salt = bcrypt.genSaltSync(4)
   // let hash = bcrypt.hashSync(req.body.password, salt)
-  //  knex('users').insert({
-  //
-  //  }, '*')
-  //  .then(user = > {
-  //    res.status(204).send({
-  //
-  //    })
-  //  })
+   knex('users').insert({
+     first_name: req.body.firstName,
+     last_name: req.body.lastName,
+     age: req.body.age,
+     weight: req.body.weight
+     cycle_length: req.body.cycleLength,
+     last_day: req.body.lastDay,
+     email: req.body.email,
+     password_hash: hash,
+     profile_pic: req.body.profilePic,
+     bio: req.body.bio,
+     certifications: req.body.certifications,
+     trainer_id: req.body.trainer_id,
+     is_trainer: req.body.isTrainer,
+     is_public: req.body.isPublic,
+     non_hormonal: req.body.isNonHormonal,
+     triphasic: req.body.isTriphasic,
+     monophasic: req.body.isMonophasic,
+     progestin: req.body.isProgestin
+   }, '*')
+   .then(user => {
+     res.status(204).send(user)
+   })
 }
 
 
 module.exports = {
   getAllUsers,
-  getHormones,
   getAllTrainers,
   getAllWorkoutsForUser,
   getWorkoutsForUserByDate,
   getUserByID,
   getTrainerByID,
+  getAllMonophasic,
+  getAllNonHormonal,
+  getAllTriphasic,
+  getAllProgestin,
+  getMonophasicById,
+  getNonHormonalById,
+  getTriphasicById,
+  getProgestinById,
+  getAllPhaseTips,
+  getPhaseTipsById,
   createUser
 }
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -8,19 +8,26 @@ router.use(express.static('public'))
 
 //GET
 router.get('/users/', queries.getAllUsers)
-router.get('/hormones/',queries.getHormones)
+router.get('/hormones/monophasic',queries.getAllMonophasic)
+router.get('/hormones/non_hormonal',queries.getAllNonHormonal)
+router.get('/hormones/triphasic',queries.getAllTriphasic)
+router.get('/hormones/progestin',queries.getAllProgestin)
 router.get('/users/trainers/', queries.getAllTrainers)
 router.get('/users/:id/workouts', queries.getAllWorkoutsForUser)
-router.get('/users/:id/workouts/date', queries.getWorkoutsForUserByDate)
+router.get('/phase_tips/',queries.getAllPhaseTips)
+// router.get('/users/:id/workouts/date', queries.getWorkoutsForUserByDate)
 
 //GET by id:
 router.get('/users/:id', queries.getUserByID)
-// router.get('users/trainers/:id', queries.getTrainerByID)
+router.get('/users/trainers/:id', queries.getTrainerByID)
+router.get('/hormones/monophasic/:id',queries.getMonophasicById)
+router.get('/hormones/non_hormonal/:id',queries.getNonHormonalById)
+router.get('/hormones/triphasic/:id',queries.getTriphasicById)
+router.get('/hormones/progestin/:id',queries.getProgestinById)
+router.get('/phase_tips/:id',queries.getPhaseTipsById)
 
 //POST
 router.post('/users/', queries.createUser)
-
-
-
+router.post('/users/:id/workouts/', queries.createWorkout)
 
 module.exports = router
