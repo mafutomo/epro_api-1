5d61b862bef350c1e94e9ae997fd88f451dd3a8d
Stephanie Marvez <marvez@gmail.com>
2018-03-04 19:01:26 -0700

    seeded phase_tips and updated string character limit

--- migrations/20180302141445_phase_tips.js
+++ migrations/20180302141445_phase_tips.js
@@ -3,8 +3,8 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('phase_tips', table => {
     table.increments()
     table.string('phase').notNullable()
-    table.string('exercise_tip').notNullable()
-    table.string('nutrition_tip').notNullable()
+    table.string('exercise_tip', [1000]).notNullable()
+    table.string('nutrition_tip', [1000]).notNullable()
     table.timestamp('created_at').notNullable().defaultsTo(knex.raw('now()'))
     table.timestamp('updated_at').notNullable().defaultsTo(knex.raw('now()'))
   })
