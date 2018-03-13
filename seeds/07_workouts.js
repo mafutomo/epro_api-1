25138b41fb4ce40db8394258f0283dd35579ec41
Melissa Utomo <mafutomo@gmail.com>
2018-03-13 15:46:02 -0600

    created join table seed and renamed seeds to numeric order

--- seeds/07_workouts.js
+++ seeds/07_workouts.js
@@ -7,7 +7,8 @@ exports.seed = function (knex, Promise) {
       return knex('workouts').insert([
         { id: 1, client_id: 1, trainer_id: 1, date: '03/05/18', name: 'Power Hour', created_by_trainer: true },
         { id: 2, client_id: 2, date: '03/05/18', name: 'Fitness for Feminists', created_by_trainer: false },
-        { id: 3, client_id: 3, trainer_id: 1, date: '03/05/18', name: 'Triples', created_by_trainer: true }
+        { id: 3, client_id: 3, trainer_id: 1, date: '03/05/18', name: 'Triples', created_by_trainer: true },
+        { id: 4, client_id: 3, trainer_id: 1, date: '03/05/18', name: 'Quadruples', created_by_trainer: true }
       ])
     }).then(() => {
       return knex.raw(
